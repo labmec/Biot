661d71c8891cb088ac7eddd0375dab9c2c176033
MMMeneghel <marinamantovanimeneghel@gmail.com>
2026-03-11 17:29:35 -0300

    Initial Commit

--- sources/TPZName.cpp
+++ sources/TPZName.cpp
@@ -1,18 +1,6 @@
 #include "TPZName.h"
-#include <iostream>
 
-TPZName::TPZName() : m_name("Unnamed") {}
-
-TPZName::TPZName(const std::string& name) : m_name(name) {}
-
-void TPZName::setName(const std::string& name) {
-    m_name = name;
-}
-
-std::string TPZName::getName() const {
-    return m_name;
-}
-
-void TPZName::printName() const {
-    std::cout << "Name: " << m_name << std::endl;
+void CreateMaterial(){
+    TPZMixedDarcyFlow *matDarcy = new TPZMixedDarcyFlow(1,2);
+    std::cout << "The End \n";
 }
--- sources/TPZName.h
+++ sources/TPZName.h
@@ -2,17 +2,22 @@
 #define TPZNAME_H
 
 #include <string>
+#include <iostream>
+#include <TPZGenGrid3D.h>
+#include <TPZGenGrid2D.h>
+#include <DarcyFlow/TPZDarcyFlow.h>
+#include <DarcyFlow/TPZMixedDarcyFlow.h>
+#include <TPZLinearAnalysis.h>
+#include "TPZSSpStructMatrix.h"
+#include "pzstepsolver.h"
+#include "TPZMultiphysicsCompMesh.h"
+#include "TPZSimpleTimer.h"
+#include "pzbuildmultiphysicsmesh.h"
+#include "TPZVTKGenerator.h"
+#include "TPZStructMatrixOMPorTBB.h"
+#include "pzskylstrmatrix.h"
+#include "TPZVTKGeoMesh.h"
 
-class TPZName {
-public:
-    TPZName();
-    TPZName(const std::string& name);
-    
-    void setName(const std::string& name);
-    std::string getName() const;
-    
-private:
-    std::string m_name;
-};
+void CreateMaterial();
 
 #endif 
